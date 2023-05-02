a1b5dc08ea2e68c50c44d41e70c9c764f190bd31
Randall Wang <randallwang93@gmail.com>
2023-05-01 23:28:38 -0700

    finish the project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # multiblockchainhub
+
+To set up the repository and run the marketplace locally, run the below
+
+```bash
+cd NFT-Marketplace-Tutorial
+npm install
+npm start
---
+++ hardhat.config.js
@@ -0,0 +1,36 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+require('dotenv').config();
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/i6G0A8cnYLorHm8GPylo_QA-81Y5yKBQ",
+      accounts: [ "d0843abb31d536bea4293ef8e7298bba5a94952351bc601b1421911c2744656d" ]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
